661d41ce0e4e0f428bdc51f7a3d9fb317069dff9
Thomas Staub <thomasstaub@Thomass-MacBook-Pro.local>
2018-04-04 21:38:14 -0400

    added character divs

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+
+
+for (var i = 0; i < 4; i++)  {
+   
+    var characterSelection  = $("<div>");
+   
+    characterSelection.addClass("character-div");
+
+    $(".container").append(characterSelection);
+
+}
